f29e936bf9d469674b1b066670a94da6ab9f52c9
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-08-17 17:58:50 +0300

    feat: create enter page

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import './css/style.css';
 import './css/normalise.css';
 import { createMainPage } from './components/main';
 import { renderRegisterPage } from './components/views/register/registerPage'
+import { renderEnterPage } from './components/views/enter/enterPage'
 
 createMainPage();
 renderRegisterPage()
+renderEnterPage()
