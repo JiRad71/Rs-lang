25f09f67adb8ea2f6ec564587f9a0094c0d046f0
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-08-17 18:07:54 +0300

    feat: create textbook page

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import './css/normalise.css';
 import { createMainPage } from './components/main';
 import { renderRegisterPage } from './components/views/register/registerPage'
 import { renderEnterPage } from './components/views/enter/enterPage'
+import { renderTextbookPage } from './components/views/textbook/textbookPage'
 
 createMainPage();
 renderRegisterPage()
 renderEnterPage()
+renderTextbookPage()
