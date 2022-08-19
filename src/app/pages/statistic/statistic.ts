4a1d5c7b487e2a29b63a9126be17ccc58b53450b
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-08-19 23:55:37 +0300

    feat: create term statistics layout

--- src/app/pages/statistic/statistic.ts
+++ src/app/pages/statistic/statistic.ts
@@ -43,6 +43,23 @@ class Statistic extends Component {
     const td11 = new Component(tr6.node, 'td', '', 'Самая длинная серия правильных ответов');
     const td12 = new Component(tr6.node, 'td', '', '0');
 
+
+
+
+
+
+
+    const statisticsTerm = new Component(this.node, 'div', 'statistics-term');
+
+    const staisticsWordsDay = new Component(statisticsTerm.node, 'div', 'ctatistic__words-day statistics__item');
+    const titleH3WodsDay = new Component(staisticsWordsDay.node, 'h3', '', 'Количество новых слов за день');
+    const canvasWordsDay = new Component(staisticsWordsDay.node, 'canvas', 'words-day statistics-canvas');
+
+
+    const staisticsWordsAll = new Component(statisticsTerm.node, 'div', 'staistics__words statistics__item');
+    const titleH3WordsAll = new Component(staisticsWordsAll.node, 'h3', '', 'Всего изучено слов');
+    const canvasWordsAll = new Component(staisticsWordsAll.node, 'canvas', 'words-all statistics-canvas');
+
   }
 }
 
