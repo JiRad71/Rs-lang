f35538abff490417f9d72126032213faff90e265
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-08-31 13:17:47 +0300

    feat: add count right and wrong answers

--- src/app/pages/audiocall/gameOverPage.ts
+++ src/app/pages/audiocall/gameOverPage.ts
@@ -18,6 +18,8 @@ class GameOverPage extends Component {
     super(parentNode);
     this.game = new Component(this.node, 'div', 'game');
     const header = new Component(this.game.node, 'h2', 'heading', 'Статистика')
+    let countRight = 0
+    let countWrong = 0
 
     const resultTable = new Component(this.game.node, 'table', '', '')
     this.resultTr = new Component(resultTable.node, 'tr', '', '')
@@ -28,9 +30,10 @@ class GameOverPage extends Component {
       return it.rightAnswer.translate === it.userAnswer.translate ? it.rightAnswer : it.userAnswer
     })
 
+
     results.forEach(i => {
       const voiceRight = new Audio(`https://rss-lang-backends.herokuapp.com/${i.rightAnswer.voice}`)
-      const voiceWrong = new Audio(`https://rss-lang-backends.herokuapp.com/${i.userAnswer.voice}`)
+
       if (i.rightAnswer.translate === i.userAnswer.translate) {
         this.resultTr = new Component(resultTable.node, 'tr', 'rightAnswer', '')
         new Component(this.resultTr.node, 'td', '', `${i.rightAnswer.translate}`)
@@ -41,7 +44,16 @@ class GameOverPage extends Component {
         }
         voiceRightAnswer.node.append(voiceRight)
         new Component(this.resultTr.node, 'td', '', 'Знаю')
-      } else {
+        countRight++
+
+      }
+    })
+
+    results.forEach(i => {
+      const voiceWrong = new Audio(`https://rss-lang-backends.herokuapp.com/${i.userAnswer.voice}`)
+
+
+      if (i.rightAnswer.translate !== i.userAnswer.translate) {
         this.resultTr = new Component(resultTable.node, 'tr', 'falseAnswer', '')
         new Component(this.resultTr.node, 'td', '', `${i.userAnswer.translate}`)
         new Component(this.resultTr.node, 'td', '', `${i.userAnswer.word}`)
@@ -51,9 +63,11 @@ class GameOverPage extends Component {
         }
         voiceWrongAnswer.node.append(voiceWrong)
         new Component(this.resultTr.node, 'td', '', 'Не знаю')
+        countWrong++
       }
-
     })
+    const countRightComponent = new Component(this.game.node, 'div', 'count-right', `${countRight}`);
+    const countWrongComponent = new Component(this.game.node, 'div', 'count-Wrong', `${countWrong}`);
 
     const categoriesButton = new Component(this.node, 'button', 'category-button', 'К выбору категории')
     categoriesButton.node.onclick = () => this.onCategories('categories')
